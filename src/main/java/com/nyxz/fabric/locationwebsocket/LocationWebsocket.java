dddee1970f05d128a6e23b2cd4a163a6fb02d1a1
pepijn <pepijn@nyxz.nl>
2025-12-06 03:25:59 +0100

    Added a try catch for any exceptions so that it wont fully crash

--- src/main/java/com/nyxz/fabric/locationwebsocket/LocationWebsocket.java
+++ src/main/java/com/nyxz/fabric/locationwebsocket/LocationWebsocket.java
@@ -6,12 +6,10 @@ import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class LocationWebsocket implements ModInitializer {
-	public static final String MOD_ID = "locationwebsocket";
+public class LocationWebSocket implements ModInitializer {
+	public static final String MOD_ID = "LocationWebSocket";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -20,16 +18,24 @@ public class LocationWebsocket implements ModInitializer {
 
     public static Config CONFIG;
 
+    /**
+     * This method is called when the mod is initialized.
+     */
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
 		LOGGER.info("Mod initialized!");
 
-        Path configPath = FabricLoader.getInstance().getConfigDir().resolve("settings.json");
-        LocationWebsocket.CONFIG = Config.load(configPath);
+
+        try {
+            Path configPath = FabricLoader.getInstance().getConfigDir().resolve("LocationWebsocket_settings.json");
+            LocationWebSocket.CONFIG = Config.load(configPath);
+        } catch (Exception e) {
+            LOGGER.error("Failed to load config: " + e.getMessage());
+            LOGGER.error("Mod will not function correctly (or at all) without a valid config.");
+        }
 
     }
 }
