7dfae54ab15cc8230a9e6777a8fa5c0a2454baf8
pepijn <pepijn@nyxz.nl>
2025-12-06 01:42:10 +0100

    Removed template lines and added loading of config file

--- src/main/java/com/nyxz/fabric/locationwebsocket/LocationWebsocket.java
+++ src/main/java/com/nyxz/fabric/locationwebsocket/LocationWebsocket.java
@@ -2,8 +2,13 @@ package com.nyxz.fabric.locationwebsocket;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class LocationWebsocket implements ModInitializer {
 	public static final String MOD_ID = "locationwebsocket";
@@ -13,12 +18,18 @@ public class LocationWebsocket implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    public static Config CONFIG;
+
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-	}
+		LOGGER.info("Mod initialized!");
+
+        Path configPath = FabricLoader.getInstance().getConfigDir().resolve("settings.json");
+        LocationWebsocket.CONFIG = Config.load(configPath);
+
+    }
 }
