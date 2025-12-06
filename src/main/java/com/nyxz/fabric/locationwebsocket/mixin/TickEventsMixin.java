c6583680f25663d20bb615b733885ca893eed536
pepijn <pepijn@nyxz.nl>
2025-12-06 03:26:38 +0100

    Deleted sendLocation (in another file now) Andadded a try catch for exceptions. Also implemented using threads

--- src/main/java/com/nyxz/fabric/locationwebsocket/mixin/TickEventsMixin.java
+++ src/main/java/com/nyxz/fabric/locationwebsocket/mixin/TickEventsMixin.java
@@ -1,28 +1,39 @@
 package com.nyxz.fabric.locationwebsocket.mixin;
 
-import com.nyxz.fabric.locationwebsocket.LocationWebsocket;
-import com.nyxz.fabric.locationwebsocket.annotations.POST;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import com.nyxz.fabric.locationwebsocket.LocationWebSocket;
+import com.nyxz.fabric.locationwebsocket.handler.Location;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MinecraftServer.class)
 public class TickEventsMixin {
+
+    /**
+     * Initializes the Location handler and registers a tick event to send player locations via WebSocket.
+     * @param info CallbackInfo provided by the mixin framework.
+     */
 	@Inject(at = @At("HEAD"), method = "loadLevel")
 	private void init(CallbackInfo info) {
-        LocationWebsocket.LOGGER.info("World initialized on server and ready to send tick events.");
-        ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            sendLocation();
+        try {
+            LocationWebSocket.LOGGER.info("World initialized on server and ready to send tick events.");
+            Location.server = (MinecraftServer) (Object) this;
+        }
+        catch (Exception e) {
+            LocationWebSocket.LOGGER.error("Error during world initialization: " + e.getMessage());
+        }
+        ServerTickEvents.START_WORLD_TICK.register(client -> {
+            try {
+                Thread thread = new Thread(new Location());
+                thread.start();
+            }
+            catch (Exception e) {
+                LocationWebSocket.LOGGER.error("Error during tick event: " + e.getMessage());
+            }
         });
 	}
 
-    @Unique
-    @POST
-    private void sendLocation() {
-        
-    }
 }
